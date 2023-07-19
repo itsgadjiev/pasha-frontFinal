bb4fcea3166a10041a87ae98715af52eaa2eb074
JekaH1 <ceyhun100203@gmail.com>
2023-07-19 20:29:14 +0400

    online business page added

--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
 // @ts-nocheck
-// $('.owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 10,
-//     nav: true,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 1
-//         },
-//         1000: {
-//             items: 1
-//         }
-//     }
-// })
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
 
 
